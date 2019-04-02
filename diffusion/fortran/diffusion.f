147d549f23a6f2adc5d6373c79bbc3db44b0303e
SilentNauscopy <babartosz.bns@gmail.com>
2019-04-03 00:24:31 +0200

    Diffusion model is done.

--- diffusion/fortran/diffusion.f
+++ diffusion/fortran/diffusion.f
@@ -1,144 +1,146 @@
       implicit none
+      ! placeholder for random calls
       REAL :: random
-      integer, parameter :: L=20            ! size of lattice
-      integer, parameter :: S=10            ! number of realization
-      integer :: K, MCS=50                ! steps
-      integer, parameter ::N=400            ! particles
-      integer, dimension(L) :: IN, IP       ! next and previous index 
-      logical, dimension(L,L) :: A          ! if occupies?
-      integer :: i,j,ii                   ! loops iterator
-      integer, dimension(N) :: x, y         ! point location
+      ! size of the lattice
+      integer, parameter :: L=20
+      ! number of realizations
+      integer, parameter :: S=20
+      ! number of max monte carlo steps
+      integer :: K, MCS=50
+      ! denisty of the system
+      real :: C=0.0
+      ! number of particles
+      integer :: N = 0
+      ! table of nearest neighbour
+      integer, dimension(L) :: IN, IP
+      ! table of occupies locations
+      logical, dimension(L,L) :: A
+      ! iterator variables
+      integer :: i,j,ii
+      ! actual location for all particles
+      integer, dimension(:), allocatable :: x, y
+      ! temporary variables
       integer :: xt, yt, dxt, dyt
-      integer, dimension(N) :: dx, dy
+      ! dispacement arrays
+      integer, dimension(:), allocatable :: dx, dy
+      ! square dispacements
       real :: dr=0, drS = 0
 
-      ! initialization of index arrays
+      open(unit=2, file='results1.csv')
+      write(2,*) "C, MCS, D"
+
+      open(unit=3, file='results2.csv')
+      write(3,*) "C, D"
+
       do i=1,L
         IN(i) = i+1
         IP(i) = i-1
       enddo
       IN(L) = 1
       IP(1) = L
+
+      ! start loop for different 
+      ! density of the gas
+      do C=0.1,1,0.1
+        N = int(C*L**2)
+        allocate(x(N), y(N))
+        allocate(dx(N), dy(N))
      
-      ! print *, IN
-      ! print *,"**********************"
-      ! print *, IP
-      print *,(N/float(L*L))
-
-      open(unit=2, file="max1.csv")
-      write(2,*) "C ,", (N/float(L*L))
-
-      do k=1,MCS
-      ! this should be for every realization 
-      drS = 0
-      do ii=1,S
-      ! initialize displacement array
-      do i=1,N
-        dx(i) = 0
-        dy(i) = 0
-      enddo
-
-      ! initialize A array
-      do i=1,L
-        do j=1,L
-            A(i,j) = .TRUE.
-        enddo
-      enddo
-
-      ! initialize x,y for all particles
-      i = N
-      do while ( i .ge. 1)
-        call random_number(random)
-        xt = int(L*random) + 1
-        call random_number(random)
-        yt = int(L*random) + 1
-        if (A(xt, yt)) then
-            A(xt, yt) = .FALSE.
-            x(i) = xt
-            y(i) = yt
-            i = i-1
-        endif
-      enddo
-
-      ! call print2d(A, L)
-      ! print *, "Prticles positions beggining"
-      ! call positions(x, y, n)
-
-      do i=1,K                              ! for every step
-          do j=1,N                          ! for every particle
-              xt = x(j)
-              yt = y(j)
-              dxt = 0
-              dyt = 0
-              call random_number(random)
-              SELECT CASE (int(4*random))
-                CASE (0)
-                    xt = IN(x(j))
-                    dxt = 1 
-                CASE (1)
-                    xt = IP(x(j))
-                    dxt = -1
-                CASE (2)
-                    yt = IN(y(j))
-                    dyt = 1
-                CASE (3)
-                    yt = IP(y(j))
-                    dyt = -1
-              ENDSELECT
-              if (A(xt,yt)) then
-                  A(xt, yt) = .FALSE.
-                  A(x(j), y(j)) = .TRUE.
-                  x(j) = xt
-                  y(j) = yt
-                  dx(j) = dx(j) + dxt
-                  dy(j) = dy(j) + dyt
-              endif
+        ! start monte carlo simulation
+        ! with different values of steps
+        do k=1,MCS
+          drS = 0
+          ! start realization
+          do ii=1,S
+          ! initialize dispacement arrays
+          do i=1,N
+            dx(i) = 0
+            dy(i) = 0
           enddo
-      enddo
 
-      dr = 0
-      do i=1,n
-        dr = dr + dx(i)**2 + dy(i)**2
-      enddo
-      ! average for 1 simulation for N particles
-      dr = dr/float(n)
-      drS = drS + dr
-      ! print *, dr
+          ! initialize A array
+          do i=1,L
+            do j=1,L
+                A(i,j) = .TRUE.
+            enddo
+          enddo
 
-      ! end loop for many simluations
-      enddo
-      !average drS for simulations
-      drS = drS/float(S)
-      ! print *, K,drS/(4 * K)
-      ! write(2,*) K,",",drS/(4 * K)
-      
+          ! initialize x,y for all particles
+          i = N
+          do while ( i .ge. 1)
+            call random_number(random)
+            xt = int(L*random) + 1
+            call random_number(random)
+            yt = int(L*random) + 1
+            if (A(xt, yt)) then
+                A(xt, yt) = .FALSE.
+                x(i) = xt
+                y(i) = yt
+                i = i-1
+            endif
+          enddo
 
+          ! start monte carlo simulation
+          ! for given K
+          do i=1,K
+              ! try to move every particle
+              do j=1,N
+                  xt = x(j)
+                  yt = y(j)
+                  dxt = 0
+                  dyt = 0
+                  call random_number(random)
+                  SELECT CASE (int(4*random))
+                    CASE (0)
+                        xt = IN(x(j))
+                        dxt = 1 
+                    CASE (1)
+                        xt = IP(x(j))
+                        dxt = -1
+                    CASE (2)
+                        yt = IN(y(j))
+                        dyt = 1
+                    CASE (3)
+                        yt = IP(y(j))
+                        dyt = -1
+                  ENDSELECT
+                  if (A(xt,yt)) then
+                      A(xt, yt) = .FALSE.
+                      A(x(j), y(j)) = .TRUE.
+                      x(j) = xt
+                      y(j) = yt
+                      dx(j) = dx(j) + dxt
+                      dy(j) = dy(j) + dyt
+                  endif
+              enddo
+          enddo
+          ! end of the monte carlo simulation
+          ! for a given K
+
+          ! compute square displacement
+          dr = 0
+          do i=1,n
+            dr = dr + dx(i)**2 + dy(i)**2
+          enddo
+          ! average for 1 simulation for N particles
+          dr = dr/float(n)
+          drS = drS + dr
+
+          enddo
+          ! end loop for many simluations
+
+          !average drS for simulations
+          drS = drS/float(S)
+          drS = drS/float(4 * K)
+          write(2,*) C, ',', K, ',', drS
+
+        enddo
+        ! end of the monte carlo simulation
+        ! for different values of steps
+
+        write(3,*) C, ',', drs
+        deallocate(x, y, dx, dy)
       enddo
-      print *, K,drS/(4 * K)
       close(2)
-      ! print *, "Perticle positions after K steps"
-      ! call positions(x, y, n)
-      ! ! displacement after K steps for N particles
-      ! print *, "Displaycment"
-      ! call positions(dx, dy, N)
+      close(3)
       end
-
-      subroutine positions(x, y, n)
-      implicit none
-      integer N
-      integer, dimension(N) :: x, y
-      integer :: i
-      do i=1,N
-        print *,i,',',x(i),',',y(i)
-      enddo
-      end subroutine positions
-
-      subroutine print2d(x, n)
-      implicit none
-      integer N
-      integer, dimension(N,N) :: x
-      integer :: i
-      do i=1,N
-        print *,x(i, :)
-      enddo
-      end subroutine print2d
